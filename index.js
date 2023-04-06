898152f4f6f57db511c70a77343107a614166636
l0st-idi0t <carlnod121@gmail.com>
2023-04-06 13:49:31 -0500

    folder draggable now

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const clock = document.getElementById('clock');
 const folder = document.getElementById('portfolio-folder');
 let clicks = 0;
+let isDragging = false;
+let dragOffset = {x: 0, y: 0};
+let folderOffset = {x: 0, y: 0};
+let folderSize = {width: folder.offsetWidth, height: folder.offsetHeight};
+let ghostFolder = null;
 
 folder.addEventListener('click', () => {
     clicks++;
@@ -18,6 +23,50 @@ document.addEventListener('click', (event) => {
         clicks = 0;
     }
 });
+
+folder.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    dragOffset.x = event.clientX;
+    dragOffset.y = event.clientY;
+    folderOffset.x = folder.offsetLeft;
+    folderOffset.y = folder.offsetTop;
+    
+    // Create a ghost folder with the same size as the original folder
+    ghostFolder = document.createElement('div');
+    ghostFolder.classList.add('folder', 'ghost');
+    ghostFolder.style.width = folderSize.width + 'px';
+    ghostFolder.style.height = folderSize.height + 'px';
+    ghostFolder.style.top = folderOffset.y + 'px';
+    ghostFolder.style.left = folderOffset.x + 'px';
+    document.body.appendChild(ghostFolder);
+    
+    // Disable text selection while dragging
+    document.body.style.userSelect = 'none';
+  });
+  
+  document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+      // Move the ghost folder with the mouse
+      ghostFolder.style.top = folderOffset.y + event.clientY - dragOffset.y + 'px';
+      ghostFolder.style.left = folderOffset.x + event.clientX - dragOffset.x + 'px';
+    }
+  });
+  
+  document.addEventListener('mouseup', (event) => {
+    if (isDragging) {
+      // Remove the ghost folder
+      ghostFolder.parentNode.removeChild(ghostFolder);
+      
+      // Enable text selection
+      document.body.style.userSelect = 'auto';
+      
+      // Move the original folder to the final position
+      folder.style.top = folderOffset.y + event.clientY - dragOffset.y + 'px';
+      folder.style.left = folderOffset.x + event.clientX - dragOffset.x + 'px';
+      
+      isDragging = false;
+    }
+  });
 
 function formatAMPM(date) {
     let hours = date.getHours();
