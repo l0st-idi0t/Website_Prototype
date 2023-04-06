0eec5c3ef6adf4adc6e33d4358fd5cec15419bd2
l0st-idi0t <carlnod121@gmail.com>
2023-04-06 13:10:34 -0500

    folder is selectable

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-let clock = document.getElementById('clock');
+const clock = document.getElementById('clock');
+const folder = document.getElementById('portfolio-folder');
+let clicks = 0;
+
+folder.addEventListener('click', () => {
+    clicks++;
+    if (clicks == 2) {
+        clicks = 0;
+        //something
+    } else {
+        folder.classList.add('selected');
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (!folder.contains(event.target)) {
+        folder.classList.remove('selected');
+        clicks = 0;
+    }
+});
 
 function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
 }
 
